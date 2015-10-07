723c060408ffc53bced8cb22ce1311cec4fb1639
juancamilo_g <juancamilogaviriaacosta@gmail.com>
2015-10-06 21:29:03 -0500

    Almacenamiento videos convertidos en S3

--- smarttools/app/models/video.rb
+++ smarttools/app/models/video.rb
@@ -12,21 +12,42 @@ class Video < ActiveRecord::Base
   #Transcodes this video to an mp4 (h.264/aac) format
   def convert_to_mp4
     #FFMPEG.ffmpeg_binary = '/usr/local/Cellar/ffmpeg/2.8.reinstall/bin/ffmpeg'
-    options = "-acodec aac -vcodec mpeg4 -strict experimental"
-    pathAOriginal = urloriginal.clone
-    pathAOriginal[0] = ''
-    pathAOriginal = File.join(Rails.public_path, pathAOriginal) 
-    ffMovie = FFMPEG::Movie.new(pathAOriginal)
+    #options = "-acodec aac -vcodec mpeg4 -strict experimental"
+    #pathAOriginal = urloriginal.clone
+    #pathAOriginal[0] = ''
+    #pathAOriginal = File.join(Rails.public_path, pathAOriginal) 
+    #ffMovie = FFMPEG::Movie.new(pathAOriginal)
 
-    nombreVideo = SecureRandom.uuid + ".mp4"
-    carpeta = File.join(Rails.public_path, "uploaded_videos", Time.now.strftime("%Y-%m-%d"), "processed")
-    rutaAbsoluta = File.join(carpeta, nombreVideo)
+    #nombreVideo = SecureRandom.uuid + ".mp4"
+    #carpeta = File.join(Rails.public_path, "uploaded_videos", Time.now.strftime("%Y-%m-%d"), "processed")
+    #rutaAbsoluta = File.join(carpeta, nombreVideo)
+    #FileUtils.mkdir_p(carpeta)
+
+    #self.urlconvertido = File.join("/uploaded_videos", Time.now.strftime("%Y-%m-%d"), "processed", nombreVideo)
+
+    #ffMovie.transcode(rutaAbsoluta, options)
+
+
+
+
+
+    carpeta = File.join(Rails.public_path, "tmp_videos", Time.now.strftime("%Y-%m-%d"))
     FileUtils.mkdir_p(carpeta)
+    rutaAbsolutaTmp = File.join(carpeta, SecureRandom.uuid + ".mp4")
+    download = open(self.videooriginals3)
+    IO.copy_stream(download, rutaAbsolutaTmp)
 
-    self.urlconvertido = File.join("/uploaded_videos", Time.now.strftime("%Y-%m-%d"), "processed", nombreVideo)
+    rutaAbsolutaConvertido = File.join(carpeta, SecureRandom.uuid + ".mp4")
+    ffMovie = FFMPEG::Movie.new(rutaAbsolutaTmp)
+    ffMovie.transcode(rutaAbsolutaConvertido, "-acodec aac -vcodec mpeg4 -strict experimental")
 
-    ffMovie.transcode(rutaAbsoluta, options)
+    AWS.config(access_key_id: ENV['KEY'], secret_access_key: ENV['SECRET'], region: 'us-west-2')
+    s3 = AWS::S3.new
+    archivo = File.basename(rutaAbsolutaConvertido)
+    rutaS3 = 'videos/processed/' + Time.now.strftime("%Y-%m-%d") + "/" + archivo
+    s3.buckets[ENV['BUCKET']].objects[rutaS3].write(:file => rutaAbsolutaConvertido)
 
+    self.urlconvertido = 'https://s3-us-west-2.amazonaws.com/smarttoolscloud/' + rutaS3
     self.estado = 'proc'
     self.save
 
