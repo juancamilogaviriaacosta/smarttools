cee2bfa2f6e9c8c74dc6108171445cc78814d6e0
Ubuntu <ubuntu@ip-172-31-44-105.us-west-2.compute.internal>
2015-10-29 17:32:31 +0000

    key management SQS

--- smarttools/config/initializers/delayed_job.rb
+++ smarttools/config/initializers/delayed_job.rb
@@ -1,6 +1,6 @@
 Delayed::Worker.configure do |config|
   # optional params:
- # config.aws_config = '~/stuff/things/aws.yml' # Specify the file location of the AWS configuration YAML if you're not using Rails and you want to use a YAML file instead of calling AWS.config
+  config.aws_config = '/home/ubuntu/aws.yaml' # Specify the file location of the AWS configuration YAML if you're not using Rails and you want to use a YAML file instead of calling AWS.config
   config.default_queue_name = 'Smart_Tools' # Specify an alternative default queue name
   #config.delay_seconds = # Sets the default delay in seconds for messages sent to the queue.
   #config.message_retention_period = 345600 # The number of seconds Amazon SQS retains a message. Must be an integer from 3600 (1 hour) to 1209600 (14 days). The default for this attribute is 345600 (4 days).
