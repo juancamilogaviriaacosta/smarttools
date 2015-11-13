47130bd686dad253d5d3626be815e7067846f623
Emanuel K <ekrivoy@gmail.com>
2015-11-13 18:23:29 -0500

    Mail config

--- smarttools/config/environments/production.rb
+++ smarttools/config/environments/production.rb
@@ -79,17 +79,17 @@ Rails.application.configure do
 
   config.assets.raise_runtime_errors = true
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  address:              'email-smtp.us-west-2.amazonaws.com',
-  port:                 587,
-  domain:               'gmail.com',
-  user_name:            ENV['SMARTTOOLS_SES_USER'],
-  password:             ENV['SMARTTOOLS_SES_PASS'],
-  authentication:       :login,
-  enable_starttls_auto: true  
-  }
-  config.action_mailer.perform_deliveries = true
+  #config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.smtp_settings = {
+  #address:              'email-smtp.us-west-2.amazonaws.com',
+  #port:                 587,
+  #domain:               'gmail.com',
+  #user_name:            ENV['SMARTTOOLS_SES_USER'],
+  #password:             ENV['SMARTTOOLS_SES_PASS'],
+  #authentication:       :login,
+  #enable_starttls_auto: true  
+  #}
+  #config.action_mailer.perform_deliveries = true
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
