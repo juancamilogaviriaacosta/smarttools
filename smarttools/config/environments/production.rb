f3a8aea14276f90f94c81d96be4dfbeed2fc8c84
Emanuel K <ekrivoy@gmail.com>
2015-11-13 19:02:34 -0500

    session handling

--- smarttools/config/environments/production.rb
+++ smarttools/config/environments/production.rb
@@ -103,8 +103,12 @@ Rails.application.configure do
     }
   }
 
+	
+  if ENV["MEMCACHEDCLOUD_SERVERS"]
+    config.cache_store = :dalli_store, ENV["MEMCACHEDCLOUD_SERVERS"].split(','), { :username => ENV["MEMCACHEDCLOUD_USERNAME"], :password => ENV["MEMCACHEDCLOUD_PASSWORD"] }
+  end
 
-  config.cache_store = :dalli_store, ENV['SMARTTOOLS_CACHE_ENDPOINT']
+  #config.cache_store = :dalli_store, ENV['SMARTTOOLS_CACHE_ENDPOINT']
 
   #config.action_controller.asset_host = ENV['CLOUDFRONT_DIST']
 end
